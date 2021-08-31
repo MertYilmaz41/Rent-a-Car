ed2534d3c082e7efde85bb9d08df2809d14894b9
Mert Yılmaz <b202102029@subu.edu.tr>
2021-09-01 00:41:10 +0300

    .

--- Core/Business.Abstract/IServiceRepository.cs
+++ Core/Business.Abstract/IServiceRepository.cs
@@ -1,11 +1,12 @@
-﻿using Core.Utilities.Results.Abstract;
+﻿using Core.Entities;
+using Core.Utilities.Results.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Business.Abstract
 {
-    public interface IServiceRepository<T>
+    public interface IServiceRepository<T> where T : class, IEntity, new()
     {
         IDataResult<List<T>> GetAll();
         IDataResult<T> GetById(int id);
